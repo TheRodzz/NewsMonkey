7a2ae89ea538f58f7c4bab330e9be0590af2016e
Vidhu Arora <vidhu.arora@iiitb.ac.in>
2023-06-21 11:18:27 +0530

    Initial commit

---
+++ src/components/News.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
